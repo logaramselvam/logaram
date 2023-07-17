aebcb86b2190c0b014f33f4f47705d19a705c699
logaramselvam <136710022+logaramselvam@users.noreply.github.com>
2023-07-17 21:01:50 +0530

    Add files via upload

---
+++ ctegorized person into height.py
@@ -0,0 +1,6 @@
+
+height_cm = int(input("Enter the height in centimeters: "))
+if height_cm <= 135:
+    print("The person is categorized as a Dwarf.")
+else:
+    print("The person is not categorized as a Dwarf.")
