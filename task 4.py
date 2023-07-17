84c5fedcae6aefa923518d9821ff5faaae2c27fe
logaramselvam <136710022+logaramselvam@users.noreply.github.com>
2023-07-17 19:45:12 +0530

    Add files via upload

---
+++ task 4.py
@@ -0,0 +1,24 @@
+week_number = int(input("Enter a week number for EG:enter the week btw 1-7: "))
+
+if week_number == 1:
+    print("Sunday")
+elif week_number == 2:
+    print("Monday")
+elif week_number == 3:
+    print("Tuesday")
+elif week_number == 4:
+    print("Wednesday")
+elif week_number == 5:
+    print("Thursday")
+elif week_number == 6:
+    print("Friday")
+elif week_number == 7:
+    print("Saturday")
+else:
+    print("wrong week number enter the correct one")
+
+
+
+
+                
+
